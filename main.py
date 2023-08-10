f615c95568962761c2bd8a956aa064a78693cc07
CaioXTSY <caio.teixeira@arapiraca.ufal.br>
2023-08-10 10:25:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from time import perf_counter
 import json
 
 def linear_search(arr, x):
-    for i in range(len(arr)):
+    if x < arr[0]:
+        return -1
+    size = arr.shape[0]
+    for i in range(size):
         if arr[i] == x:
             return i
     return -1
 
 def binary_search(arr, x):
     low = 0
-    high = len(arr) - 1
+    high = arr.shape[0] - 1
     mid = 0
 
     while low <= high:
@@ -24,8 +27,13 @@ def binary_search(arr, x):
     return -1
 
 def linear_search_file(file_obj, x):
+    file_obj.seek(0)
+    first_element = np.fromfile(file_obj, dtype=np.uint64, count=1)[0]
+    if x < first_element:
+        return -1
+
     file_obj.seek(0, 2)
-    total_elements = file_obj.tell() // 8  
+    total_elements = file_obj.tell() // 8
     file_obj.seek(0)
 
     for index in range(total_elements):
@@ -46,7 +54,7 @@ def binary_search_file(file_obj, x):
 
         if num < x:
             low = mid + 1
-        elif num < x:
+        elif num > x:
             high = mid - 1
         else:
             return mid
@@ -89,47 +97,59 @@ def main():
             break
 
         try:
-            if choice != "6" and choice != "7":
+            if choice in ["1", "2", "3", "4", "5"]:
                 desired_mb = int(input("\nDigite o tamanho desejado em MB para os dados: "))
                 num_elements = (desired_mb * 1024 * 1024) // 8
                 arr = np.arange(num_elements, dtype=np.uint64)
                 search_value = int(input("Digite o valor que deseja buscar: "))
 
-            if choice == "1":
-                avg_time, _ = performance(linear_search, (arr, search_value))
-                results["Linear Array"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
-                print(f"\nBusca Linear em Array(ram): Tempo médio: {display_time(avg_time)}s")
-            elif choice == "2":
-                avg_time, _ = performance(binary_search, (arr, search_value))
-                results["Binary Array"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
-                print(f"\nBusca Binária em Array(ram): Tempo médio: {display_time(avg_time)}s")
-            elif choice == "3":
-                path = 'data.bin'
-                arr.tofile(path)
-                with open(path, 'rb') as rf:
-                    avg_time, _ = performance(linear_search_file, (rf, search_value))
-                    results["Linear File"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
-                    print(f"\nBusca Linear em Arquivo: Tempo médio: {display_time(avg_time)}s")
-            elif choice == "4":
-                path = 'data.bin'
-                arr.tofile(path)
-                with open(path, 'rb') as rf:
-                    avg_time, _ = performance(binary_search_file, (rf, search_value))
-                    results["Binary File"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
-                    print(f"\nBusca Binária em Arquivo: Tempo médio: {display_time(avg_time)}s")
-            elif choice == "5":
-                avg_time, _ = performance(linear_search, (arr, search_value))
-                results["Linear Array"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
-                avg_time, _ = performance(binary_search, (arr, search_value))
-                results["Binary Array"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
-                path = 'data.bin'
-                arr.tofile(path)
-                with open(path, 'rb') as rf:
-                    avg_time, _ = performance(linear_search_file, (rf, search_value))
-                    results["Linear File"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
-                    avg_time, _ = performance(binary_search_file, (rf, search_value))
-                    results["Binary File"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
-                print("\nTodos os testes foram executados!")
+                if choice == "1":
+                    avg_time, result = performance(linear_search, (arr, search_value))
+                    results["Linear Array"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
+                    if result == -1:
+                        print(f"\nBusca Linear em Array(ram): O elemento não foi encontrado. (Pior caso)")
+                    else:
+                        print(f"\nBusca Linear em Array(ram): Tempo médio: {display_time(avg_time)}s")
+                elif choice == "2":
+                    avg_time, result = performance(binary_search, (arr, search_value))
+                    results["Binary Array"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
+                    if result == -1:
+                        print(f"\nBusca Binária em Array(ram): O elemento não foi encontrado. (Pior caso)")
+                    else:
+                        print(f"\nBusca Binária em Array(ram): Tempo médio: {display_time(avg_time)}s")
+                elif choice == "3":
+                    path = 'data.bin'
+                    arr.tofile(path)
+                    with open(path, 'rb') as rf:
+                        avg_time, result = performance(linear_search_file, (rf, search_value))
+                        results["Linear File"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
+                        if result == -1:
+                            print(f"\nBusca Linear em Arquivo: O elemento não foi encontrado. (Pior caso)")
+                        else:
+                            print(f"\nBusca Linear em Arquivo: Tempo médio: {display_time(avg_time)}s")
+                elif choice == "4":
+                    path = 'data.bin'
+                    arr.tofile(path)
+                    with open(path, 'rb') as rf:
+                        avg_time, result = performance(binary_search_file, (rf, search_value))
+                        results["Binary File"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
+                        if result == -1:
+                            print(f"\nBusca Binária em Arquivo: O elemento não foi encontrado. (Pior caso)")
+                        else:
+                            print(f"\nBusca Binária em Arquivo: Tempo médio: {display_time(avg_time)}s")
+                elif choice == "5":
+                    avg_time, result = performance(linear_search, (arr, search_value))
+                    results["Linear Array"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
+                    avg_time, result = performance(binary_search, (arr, search_value))
+                    results["Binary Array"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
+                    path = 'data.bin'
+                    arr.tofile(path)
+                    with open(path, 'rb') as rf:
+                        avg_time, result = performance(linear_search_file, (rf, search_value))
+                        results["Linear File"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
+                        avg_time, result = performance(binary_search_file, (rf, search_value))
+                        results["Binary File"] = {"Tempo médio": display_time(avg_time), "RAM": desired_mb, "Elemento": search_value}
+                    print("\nTodos os testes foram executados!")
             elif choice == "6":
                 print("\nResultados:")
                 for key, value in results.items():
